2f05fb42f5a249d2a1e3df8778b17c79907906ff
Joshua Taylor <49791155+jdtaylor311@users.noreply.github.com>
2023-05-28 20:37:33 -0400

    Adding recipes list details and the ability to select a recipe and display the details. also added navigation via ngif and enums for selection (#8)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { HeaderSelection } from "./types/header-selection-enum";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,16 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "recipes-and-ingredients";
+  Recipes = HeaderSelection.RECIPES;
+  ShoppingList = HeaderSelection.SHOPPING_LIST;
+  FeatureEnum = HeaderSelection;
+  selectedHeaderItem: HeaderSelection = HeaderSelection.RECIPES;
+
+  onRecipesSelected() {
+    this.selectedHeaderItem = HeaderSelection.RECIPES;
+  }
+
+  onShoppingListSelected() {
+    this.selectedHeaderItem = HeaderSelection.SHOPPING_LIST;
+  }
 }
---
+++ src/app/types/header-selection-enum.ts
@@ -0,0 +1,4 @@
+export enum HeaderSelection {
+    RECIPES = 0,
+    SHOPPING_LIST = 1
+}
